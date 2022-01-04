1b2c2c2ba7edcef280147e5102c00a2496c232ba
mohammadalimojtahedsoleimani <77240820+mohammadalimojtahedsoleimani@users.noreply.github.com>
2022-01-04 21:30:44 +0330

    btn added to local storage with id and changed innertext of btns when clicked and some changes in css

--- app.js
+++ app.js
@@ -12,21 +12,21 @@ const cartTotal = document.querySelector(".cart-total");
 const cartContent = document.querySelector(".cart-content");
 const productsDOM = document.querySelector(".products-center");
 
-
+let cart = [];
 
 // get products
 class Productes {
-  getProducts(){
-    return Products;
-  }
+    getProducts() {
+        return Products;
+    }
 }
 
 
 class UI {
-  displayProducts(produtcs){
-    let result = "";
-    produtcs.forEach(item => {
-      result += `<div class="product">
+    displayProducts(produtcs) {
+        let result = "";
+        produtcs.forEach(item => {
+            result += `<div class="product">
             <div class="img-container">
                 <img src=${item.fields.image.fields.file.url} class="product-img"  alt="images"/>
             </div>
@@ -39,47 +39,68 @@ class UI {
                 add to cart
             </button>
         </div>`;
-      productsDOM.innerHTML = result;
-    })
-  }
-  getAddToCartBtns(){
-    const addTo = document.querySelectorAll(".add-to-cart");
-    console.log(addTo);
-    const lips = [...addTo];
-    lips.forEach((btn)=>{
-      console.log(btn.dataset.id)
-    })
-    console.log(lips)
-  }
+            productsDOM.innerHTML = result;
+        })
+    }
+
+    getAddToCartBtns() {
+        const addTo = document.querySelectorAll(".add-to-cart");
+        const lips = [...addTo];
+        lips.forEach((btn) => {
+            const id = btn.dataset.id;
+            const isInCart = cart.find((p) => p.id === id);
+            if (isInCart) {
+                btn.innerText = `In Cart`;
+                btn.disabled = true;
+            }
+            btn.addEventListener('click', (event) => {
+                event.target.innerText = "In Cart";
+                event.target.disabled = false;
+                const addedProduct = Storage.getProduct(id);
+                cart = [...cart, {...addedProduct, quantity: 1}]
+                Storage.saveCart(cart);
+            })
+        })
+
+    }
 }
 
 class Storage {
-  saveProducts (products){
-    //fght be sorat string save mishe pas stringify
-    localStorage.setItem("products" ,JSON.stringify(products))
-  }
+    saveProducts(products) {
+        //fght be sorat string save mishe pas stringify
+        localStorage.setItem("products", JSON.stringify(products))
+    }
+
+    static getProduct(id) {
+        const _products = JSON.parse(localStorage.getItem("products"))
+        return _products.find((p) => p.id === parseInt(id));
+    }
+
+    static saveCart(cart) {
+        localStorage.setItem("cart", JSON.stringify(cart));
+    }
 }
 
 window.addEventListener('DOMContentLoaded', (event) => {
-  const pro = new Productes();
-const pros =   pro.getProducts();
-  const ui = new UI();
-  ui.displayProducts(pros);
-  ui.getAddToCartBtns();
-  const sto = new Storage();
-  sto.saveProducts(pros);
+    const pro = new Productes();
+    const pros = pro.getProducts();
+    const ui = new UI();
+    ui.displayProducts(pros);
+    ui.getAddToCartBtns();
+    const sto = new Storage();
+    sto.saveProducts(pros);
 });
 
 function showModalFunction() {
-  backDrop.style.display = "block";
-  cartModal.style.opacity = "1";
-  cartModal.style.top = "20%";
+    backDrop.style.display = "block";
+    cartModal.style.opacity = "1";
+    cartModal.style.top = "20%";
 }
 
 function closeModalFunction() {
-  backDrop.style.display = "none";
-  cartModal.style.opacity = "0";
-  cartModal.style.top = "-100%";
+    backDrop.style.display = "none";
+    cartModal.style.opacity = "0";
+    cartModal.style.top = "-100%";
 }
 
 cartBtn.addEventListener("click", showModalFunction);
